2a6310ff8af9269866489dc0e44e0bd0d781a8d9
Damon Garvin <damongarvin@gmail.com>
2022-05-12 01:42:08 -0400

    Uploading to grab on laptop.

--- ui-manager.js
+++ ui-manager.js
@@ -1,1 +1,13 @@
 const viewscreen = document.getElementById('viewscreen');
+
+viewscreen.classList.add('radialBackground');
+
+
+//loading screen
+//intro screen
+//enter name
+//choose character
+//travel?
+//battle
+
+
